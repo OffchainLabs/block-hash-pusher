bdc62cc85bbd7a105a46bc0f3d16d45cec74326c
Henry <11198460+godzillaba@users.noreply.github.com>
2025-04-08 10:59:43 -0500

    add batched pushing

--- contracts/Pusher.sol
+++ contracts/Pusher.sol
@@ -7,13 +7,16 @@ import {ArbitrumChecker} from "@arbitrum/nitro-contracts/src/libraries/ArbitrumC
 import {IInbox} from "@arbitrum/nitro-contracts/src/bridge/IInbox.sol";
 import {Buffer} from "./Buffer.sol";
 
-/// @notice The Pusher gets the hash of the previous block and pushes it to the buffer on the child chain via retryable ticket.
+/// @notice The Pusher gets the hash of the previous 256 blocks and pushes them to the buffer on the child chain via retryable ticket.
 contract Pusher {
     /// @notice Whether this contract is deployed on an Arbitrum chain.
     ///         This condition changes the way the block number is retrieved.
     bool public immutable isArbitrum;
     /// @notice The address of the buffer contract on the child chain.
     address public immutable bufferAddress;
+
+    /// @notice Emitted when block hashes are pushed to the buffer.
+    event BlockHashesPushed(uint256 firstBlockNumber);
 
     /// @notice Thrown when the amount of ETH sent to the contract is not equal to the specified retryable ticket cost.
     error WrongEthAmount(uint256 received, uint256 expected);
@@ -33,22 +36,12 @@ contract Pusher {
     /// @param gasLimit The gas limit for the transaction.
     /// @param submissionCost The cost of submitting the transaction.
     function pushHash(address inbox, uint256 gasPriceBid, uint256 gasLimit, uint256 submissionCost) external payable {
-        uint256 blockNumber;
-        bytes32 blockHash;
-        if (isArbitrum) {
-            blockNumber = ArbSys(address(100)).arbBlockNumber() - 1;
-            blockHash = ArbSys(address(100)).arbBlockHash(blockNumber);
-        } else {
-            blockNumber = block.number - 1;
-            blockHash = blockhash(blockNumber);
-        }
-
         if (gasPriceBid * gasLimit + submissionCost != msg.value) {
             revert WrongEthAmount(msg.value, gasPriceBid * gasLimit + submissionCost);
         }
 
-        bytes32[] memory blockHashes = new bytes32[](1);
-        blockHashes[0] = blockHash;
+        (uint256 firstBlockNumber, bytes32[] memory blockHashes) = _buildBlockHashArray();
+
         IInbox(inbox).createRetryableTicket{value: msg.value}({
             to: bufferAddress,
             l2CallValue: 0,
@@ -57,9 +50,26 @@ contract Pusher {
             callValueRefundAddress: msg.sender,
             gasLimit: gasLimit,
             maxFeePerGas: gasPriceBid,
-            data: abi.encodeCall(Buffer.receiveHashes, (blockNumber, blockHashes))
+            data: abi.encodeCall(Buffer.receiveHashes, (firstBlockNumber, blockHashes))
         });
 
-        // QUESTION: emit an event?
+        emit BlockHashesPushed(firstBlockNumber);
+    }
+
+    /// @dev Build an array of the last 256 block hashes
+    function _buildBlockHashArray() internal view returns (uint256 firstBlockNumber, bytes32[] memory blockHashes) {
+        blockHashes = new bytes32[](256);
+        if (isArbitrum) {
+            firstBlockNumber = ArbSys(address(100)).arbBlockNumber() - 256;
+            for (uint256 i = 0; i < 256; i++) {
+                blockHashes[i] = ArbSys(address(100)).arbBlockHash(firstBlockNumber + i);
+            }
+        }
+        else {
+            firstBlockNumber = block.number - 256;
+            for (uint256 i = 0; i < 256; i++) {
+                blockHashes[i] = blockhash(firstBlockNumber + i);
+            }
+        }
     }
 }
