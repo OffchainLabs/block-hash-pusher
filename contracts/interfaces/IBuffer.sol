f7c2973a59b513729f54b03b42e3a9029085b61f
Henry <11198460+godzillaba@users.noreply.github.com>
2025-05-06 09:07:40 -0500

    document buffer size

--- contracts/interfaces/IBuffer.sol
+++ contracts/interfaces/IBuffer.sol
@@ -31,9 +31,11 @@ interface IBuffer {
     /// @return The block hash of the parent block.
     function parentBlockHash(uint256 parentBlockNumber) external view returns (bytes32);
 
-    /// @dev The size of the buffer. This is the maximum number of block hashes that can be stored.
-    ///      Assuming a parent block time of 250ms and L1 block time of 12s,
-    ///      then the amount of time that the buffer covers is equivalent to EIP-2935's.
+    /// @dev 393168 - the size of the buffer. This is the maximum number of block hashes that can be stored.
+    ///      For a parent chain with a block time of 250ms, this is equivalent to roughly 27 hours of history,
+    ///      which is the same amount of time that EIP-2935 covers for L1 block hashes.
+    ///      For a parent chain with a block time of 12s, this is equivalent to roughly 54 days of history.
+    ///      For example, Arbitrum One has L1 as the parent, so the Buffer deployed to Arbitrum One will cover ~54 days of L1 history.
     function bufferSize() external view returns (uint256);
     /// @dev A system address that is authorized to push hashes to the buffer.
     function systemPusher() external view returns (address);
