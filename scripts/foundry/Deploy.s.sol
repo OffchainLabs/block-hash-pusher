a1e26f2e42e6306d1e7f03c5d20fa6aa64ff7a12
Henry <11198460+godzillaba@users.noreply.github.com>
2025-06-05 11:11:46 -0700

    improve readme and script

--- scripts/foundry/Deploy.s.sol
+++ scripts/foundry/Deploy.s.sol
@@ -8,15 +8,14 @@ import {Buffer} from "contracts/Buffer.sol";
 contract DeployScript is Script {
     function run() public {
         bytes32 salt = vm.envBytes32("CREATE2_SALT");
-        address bufferAddress =
-            Create2.computeAddress(salt, keccak256(abi.encodePacked(type(Buffer).creationCode)), CREATE2_FACTORY);
+        address bufferAddress = vm.envAddress("EXPECTED_BUFFER_ADDRESS");
         address pusherAddress =
             address(uint160(uint256(keccak256(abi.encodePacked(bytes2(0xd694), bufferAddress, bytes1(uint8(1)))))));
         if (bufferAddress.code.length != 0) {
             console.log("Already Deployed");
         } else {
             vm.startBroadcast();
-            new Buffer{salt: vm.envBytes32("CREATE2_SALT")}();
+            require(bufferAddress == address(new Buffer{salt: salt}()), "Buffer deployed to unexpected address");
             vm.stopBroadcast();
         }
         console.log("Buffer Address: ", bufferAddress);
